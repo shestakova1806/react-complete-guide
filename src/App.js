d64f51ff699fbeee75e32105f05356c803b5c562
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-07 11:23:51 +0500

    feat: added passing method references

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export class App extends Component {
     ],
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        { name: "Alexandra", age: 23 },
+        { name: newName, age: 23 },
         { name: "Masha", age: 24 },
         { name: "Kristina", age: 30 },
       ],
@@ -26,7 +26,9 @@ export class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Alexandra!!")}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -34,6 +36,7 @@ export class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Sasha!")}
         >
           My Hobbies: Racing
         </Person>
