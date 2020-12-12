ef8a9ed41e813cc2a3ef7703f93c0150fdec5f3d
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-12 14:54:14 +0300

    feat: added styles and class names dinamically

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ export class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -65,12 +66,23 @@ export class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = "red";
+    }
+
+    let classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button onClick={this.togglePesonsHandler} style={style}>
           Toggle Persons
         </button>
