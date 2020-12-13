3eb899021fca68746e2f9a34f0e8b4a5102b0937
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-13 10:22:17 +0300

    refactor: removed styles from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import "./App.css";
 import { Person } from "./Person/Person";
-
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.alt ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${(props) => (props.alt ? "salmon" : "lightgreen")};
-    color: black;
-  }
-`;
 
 export class App extends Component {
   state = {
@@ -53,19 +39,6 @@ export class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-      },
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
@@ -105,12 +78,9 @@ export class App extends Component {
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton
-          alt={this.state.showPersons}
-          onClick={this.togglePesonsHandler}
-        >
+        <button className="button" onClick={this.togglePesonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
