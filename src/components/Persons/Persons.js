f43dfafe1de82b7ebea0a515e7b0d2872b7fb4b4
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-14 23:07:54 +0300

    feat: added update lifecycle methods for props changes

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import { Person } from "./Person/Person";
 
-export const Persons = (props) => {
-  console.log("[Persons.js] rendering...");
+export class Persons extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
 
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={(event) => props.changed(event, person.id)}
-      />
-    );
-  });
-};
+  getSnapshotBeforeUpdate() {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
