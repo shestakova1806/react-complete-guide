507e8a5390fe8f7f800052f622edbe1e6a5519f1
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-18 18:02:27 +0300

    refactor: added and used aux for person

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Aux } from "../../../hoc/Aux";
 import classes from "./Person.css";
 
 export class Person extends Component {
@@ -6,7 +7,7 @@ export class Person extends Component {
     console.log("[Person.js] rendering...");
 
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -16,7 +17,7 @@ export class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
     );
   }
 }
