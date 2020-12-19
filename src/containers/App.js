bc267b5a650eee4fb01562adea3ac73f4faffbf8
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-19 15:38:08 +0300

    feat: added changeCounter and setting state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,6 +19,7 @@ class app extends Component {
     otherState: "some other value",
     showPersons: false,
     showCockpit: true,
+    changeCounter: 0,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -50,7 +51,12 @@ class app extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
+    });
   };
 
   deletePersonHandler = (personIndex) => {
